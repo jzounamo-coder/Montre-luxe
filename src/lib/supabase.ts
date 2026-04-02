7239fe74e9c4fcf932aef369f8619054a0d56219
dany <jzounamo@gmail.com>
2026-04-02 11:10:13 +0100

    modif URL

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-// On ajoute "as string" pour rassurer TypeScript
-const supabaseUrl = (import.meta.env.VITE_SUPABASE_URL as string)
-const supabaseAnonKey = (import.meta.env.VITE_SUPABASE_ANON_KEY as string)
+// On utilise les noms exacts que tu as mis dans ton .env
+const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("Attention : Les variables d'environnement Supabase sont manquantes !")
+  console.error("Attention : Les variables d'environnement Supabase sont manquantes ! Vérifiez le .env");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
